6ddb382ae4e957ca3c138e89a40f31b0f63c5aae
aarush-glitch <hgupta164@gmail.com>
2024-04-20 09:28:27 +0530

    Started Project with index files and webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBheM3NU55io2CbcH5eN16tfz9rsHyHjuM",
+    authDomain: "raktdatra-b59de.firebaseapp.com",
+    projectId: "raktdatra-b59de",
+    storageBucket: "raktdatra-b59de.appspot.com",
+    messagingSenderId: "393948811892",
+    appId: "1:393948811892:web:6ebb7f9a46ae73a3723721"
+  };
